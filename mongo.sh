d11b7a2f4af4f29e92fa9d9c912806e1468ed7ad
jaswanth kumar <jaswanthkumarjash2000@gmail.com>
2025-11-25 22:21:53 +0530

    Shell roboshop project

---
+++ mongo.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: You are not a root user to run this script"
+    exit 1
+fi
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+
+echo "Script execution started at: $(date)" | tee -a $LOG_FILE
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+VALIDATE () {
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ...$R FAILED$N" | tee -a $LOG_FILE
+        exit 2
+    else 
+        echo "$2 ...$G SUCCESS$N" | tee -a $LOG_FILE
+    fi
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Adding mongo repo"
+
+dnf install mongodb-org -y &>> $LOG_FILE
+VALIDATE $? "Installing MongoDB"
+
+systemctl enable mongod &>> $LOG_FILE
+VALIDATE $? "Enabling mongod service"
+
+systemctl start mongod
+VALIDATE $? "Starting mongod service"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connections to MongoDB"
+
+systemctl restart mongod 
+VALIDATE $? "Restarting mongod service"
