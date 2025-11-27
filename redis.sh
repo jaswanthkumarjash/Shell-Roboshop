048cdc5190f83eb9b2e13fba112dce712f65c2b7
jaswanth kumar <jaswanthkumarjash2000@gmail.com>
2025-11-27 17:13:52 +0530

    Shell script practice

--- redis.sh
+++ redis.sh
@@ -37,7 +37,7 @@ VALIDATE $? "Enabling redis version 7"
 dnf install redis -y &>> $LOG_FILE
 VALIDATE $? "Installing redis"
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no/' /etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
 VALIDATE $? "Allowing remote connections to redis and changing from protected-mode from yes to no"
 
 systemctl enable redis &>> $LOG_FILE
