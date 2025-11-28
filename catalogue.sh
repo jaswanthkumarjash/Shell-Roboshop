b949f40932d90f03a175fb5ab17e4967edb6d634
jaswanth kumar <jaswanthkumarjash2000@gmail.com>
2025-11-28 12:13:28 +0530

    Shell script practice

--- catalogue.sh
+++ catalogue.sh
@@ -83,8 +83,8 @@ VALIDATE $? "Adding mongo repo"
 dnf install mongodb-mongosh -y &>> $LOG_FILE
 VALIDATE $? "Installing mongo client"
 
-INDEX=$(mongosh mongodb.daws86s.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
-if [ $INDEX -lt 0 ]; then
+INDEX=$(mongosh mongo.daws86s.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+if [ $INDEX -le 0 ]; then
     mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Load catalogue products"
 else
